891c8bf2e0531bc756648cdd6c53f1e0dd78041c
RedwanPlague <mdredwan.rh@gmail.com>
2021-05-01 20:25:57 +0600

    cleaned garbage files

--- our-vTR/run.sh
+++ our-vTR/run.sh
@@ -1,1 +1,17 @@
-python3 train.py
+case $1 in
+    h)
+        echo 'running hypertuner'
+        python3 hypertuner.py
+    ;;
+    c)
+        echo 'running CV'
+        python3 CV.py
+    ;;
+    t)
+        echo 'running plain train'
+        python3 train.py
+    ;;
+    *)
+        echo 'invalid arguments, check inside' $0 'file'
+    ;;
+esac
